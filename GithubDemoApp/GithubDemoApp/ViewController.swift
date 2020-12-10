9593db5841ff46e77a9d28d413fb56bf2e6c7ceb
Ahmed Hamdy <hamdyA@smaat.co>
2020-12-10 02:01:16 +0200

    add search controller

--- GithubDemoApp/GithubDemoApp/ViewController.swift
+++ GithubDemoApp/GithubDemoApp/ViewController.swift
@@ -9,14 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let searchController = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         NetworkManager.getRepositoriesWithCompletion { (list) in
             print(list)
         }
+        setupSearchController()
     }
 
+    func setupSearchController() {
+        searchController.searchBar.placeholder = "Enter Repository Name"
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
 
 }
 
+
+extension ViewController: UISearchBarDelegate {
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        //TODO: return to default data
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
